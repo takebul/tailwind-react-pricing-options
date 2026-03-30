6f9379fb7a7fa12d69473ab310f3161e290f6ee4
takebul <takebulislam@gmail.com>
2026-03-31 01:47:32 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import DaisyNav from "./components/DaisyNav/DaisyNav";
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <header>
+        <Navbar></Navbar>
+        {/* <DaisyNav></DaisyNav> */}
+      </header>
+      <main></main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import Link from "./Link";
+import { Menu, X } from "lucide-react";
+
+const navData = [
+  {
+    id: 1,
+    name: "Home",
+    path: "/",
+  },
+  {
+    id: 2,
+    name: "About",
+    path: "/about",
+  },
+  {
+    id: 3,
+    name: "Services",
+    path: "/services",
+  },
+  {
+    id: 4,
+    name: "Blog",
+    path: "/blog",
+  },
+  {
+    id: 5,
+    name: "Contact",
+    path: "/contact",
+  },
+];
+
+const Navbar = () => {
+    const [open, setOpen] = useState(false);
+
+    const links = navData.map((route) => (
+          <Link key={route.id} route={route}></Link>
+        ))
+
+  return (
+    <nav className="flex justify-between mx-10 mt-4">
+      <span className="flex" onClick={() => setOpen(!open)}>
+        {open ? (
+          <X className="md:hidden"></X>
+        ) : (
+          <Menu className="md:hidden"></Menu>
+              )}
+              
+              <ul className={`md:hidden absolute z-10 duration-1000 text-black
+                ${open ? 'top-8': '-top-40'}
+                bg-amber-200`}>
+                  {links}
+              </ul>
+
+        <h3 className="ml-4">My Navbar</h3>
+      </span>
+      <ul className="md:flex hidden">
+        {
+            links    
+        }
+      </ul>
+
+      {/* <ul className="flex">
+        {navData.map((route) => (
+          <li className="mr-10">
+            <a href={route.path}>{route.name}</a>
+          </li>
+        ))}
+      </ul> */}
+
+      {/* <ul className="flex">
+        <li className="mr-10">
+          <a href="/">Home</a>
+        </li>
+        <li className="mr10">
+          <a href="/about">About</a>
+        </li>
+        <li className="ml-10">
+          <a href="/blog">Blog</a>
+        </li>
+      </ul> */}
+
+      <button>Sign In</button>
+    </nav>
+  );
+};
+
+export default Navbar;
